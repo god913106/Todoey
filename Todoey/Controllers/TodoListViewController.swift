8d23cd1183ac5d9797b72c5a1edf456bbc5fcbab
楊崇平 <god913106@gmail.com>
2018-04-11 17:47:22 +0800

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,17 +10,32 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard //android SharedPreferences
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //and we're going to cast this as an array of strings
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        
+        let newItem1 = Item()
+        newItem1.title = "Find Mike"
+        newItem1.done = true
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destory Demogorgon"
+        itemArray.append(newItem3)
+        //有個bug就是你對某row打勾了 滑到下面明明有個沒打勾的卻打勾 有重覆利用這個物件
+      
+        //and we're going to cast this as an array of strings [String] ->[Item]
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
              itemArray = items
         }
-        
+
     }
     
     //MARK - TableView Datasource Methods
@@ -30,9 +45,26 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("cellForRowAtIndex")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none //跟下面的if/else 一樣的功能 卻只要寫一行就好
+        
+        //但還有個bug你勾選後卻不會更新
+        /*
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }*/
         
         return cell
     }
@@ -41,12 +73,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // print(itemArray[indexPath.row])
         
-        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
-           tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done //同下註解掉的
         
+//        if  itemArray[indexPath.row].done == true {
+//            itemArray[indexPath.row].done = false
+//        }else{
+//            itemArray[indexPath.row].done = true
+//        }
+        
+//        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+//           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        tableView.reloadData() //
         
         //因為點選row時 會一直是灰體 直到點選另一個row 才會變回白體
         //所以用deselectRow 這個method 可以點選某row 灰體馬上回白體 為了更良好的用戶體驗
@@ -63,9 +103,12 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks the Add Itme button on our UIAlert
             print("Success!")
             print(textField.text!)
-            self.itemArray.append(textField.text!) //新增了沒錯 但沒有重載 他是不會出現的
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+           // self.itemArray.append(textField.text!) //新增了沒錯 但沒有重載 他是不會出現的
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+         //   self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()
             
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 洋蔥胖 on 2018/4/10.
+//  Copyright © 2018年 ChrisYoung. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String  = ""
+    var done : Bool = false
+    }
